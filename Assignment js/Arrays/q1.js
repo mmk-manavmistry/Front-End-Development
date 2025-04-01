2a1a544804059c3d6fd0d1c5be43c71e4a64f65d
mmk-manavmistry <126792273+mmk-manavmistry@users.noreply.github.com>
2025-04-01 11:45:36 +0530

    practical

---
+++ Assignment js/Arrays/q1.js
@@ -0,0 +1,11 @@
+//  Declare an array of fruits (["apple", "banana", "cherry"]). Use JavaScript to:
+//  Add a fruit to the end of the array.
+//  Remove the first fruit from the array.
+//  Log the modified array to the console
+let fruits = ['apple', 'banana', 'cherry'];
+fruits.push('pineapple'); // Add Fruit to the end
+fruits.pop(); // Remove the last fruit
+console.log(fruits);
+
+
+
